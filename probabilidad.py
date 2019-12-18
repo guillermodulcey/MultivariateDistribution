0f66e0b0537fedb275e2eeebf800d4b61e42064f
Karla Guzmán <karla_g97@hotmail.com>
2019-12-18 17:10:40 -0600

    Graficando y multiples variables

--- probabilidad.py
+++ probabilidad.py
@@ -3,28 +3,32 @@ import numpy
 import random
 
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.interpolate import griddata
 
 class Probabilidad():
     def __init__(self):
         self.matrizCov = []
         pass
 
-    def generarDataSet(self, mu:list, sigma:list, k, semilla):
+    def generarDataSet(self, mu:list, matrizCov:list, k, semilla):
         random.seed(semilla)
         datos = []
+        datosR = []
         numVariables = len(mu)
-        ro = self.__generarRo(numVariables)
-        self.matrizCov = self.generarMatriz(sigma,ro)
-        print(self.matrizCov) 
+        self.matrizCov = matrizCov
         for i in range(0,k):
             while True:
                 z, vector = self.generarVector(numVariables)
-                evaluacion = self.pmf(vector,mu,self.matrizCov)
+                evaluacion = self.pdf(vector,mu,self.matrizCov)
                 if z <= evaluacion:
                     datos.append(vector)
                     break
-        return datos
+                else:
+                    datosR.append(vector)
+
+        return datos, datosR
 
     def generarVector(self,longitud):
         vector = []
@@ -33,7 +37,7 @@ class Probabilidad():
         z = random.uniform(0,1)
         return z, vector
             
-    def pmf(self, x:list, mu:list, matrixSigma):
+    def pdf(self, x:list, mu:list, matrixSigma):
         k = len(x)
 
         xmu = numpy.array(x) - numpy.array(mu)
@@ -43,10 +47,8 @@ class Probabilidad():
 
         pi = math.pow(2*math.pi,(-k/2))
 
-        ####Revisar####
         determinante = numpy.linalg.det(nmatriz)
         det = math.pow(determinante,(-1/2))
-        ###############
 
         inv = numpy.linalg.inv(nmatriz)
 
@@ -57,26 +59,32 @@ class Probabilidad():
         e = math.exp(potencia)
         return pi*det*e
 
-    def generarMatriz(self, sigma:list, ro):
+    def generarMatriz(self, sigma:list, ro:list):
         matriz = []
         lFilas = len(sigma)
+        contador = 0
+        dr = list(map(self.decodificar, ro))
         for i in range(0,lFilas):
             fila = []
             for j in range(0,lFilas):
-                if i==j:
-                    fila.append(sigma[i]*sigma[j])
+                if j>=i:
+                    if i==j:
+                        fila.append(sigma[i]*sigma[j])
+                    else:
+                        fila.append(dr[contador]*sigma[i]*sigma[j])
+                        contador+=1
                 else:
-                    fila.append(ro[i][j]*sigma[i]*sigma[j])
+                    fila.append(0)
             matriz.append(fila)
 
-        for i in range(0,lFilas):
-            for j in range(0,lFilas):
-                matriz[i][j] = matriz[j][i]
+        for i in range(0,lFilas-1):
+            for j in range(i+1,lFilas):
+                matriz[j][i] = matriz[i][j]
 
         return matriz
 
     #########Bivariate
-    def pmfBivariate(self, x:list, mu:list, sigma:list, ro):
+    def pdfBivariate(self, x:list, mu:list, sigma:list, ro):
         resta = 1-math.pow(ro,2)
         raiz = math.pow(resta,(1/2))
         coeficiente = 2*math.pi*sigma[0]*sigma[1]
@@ -101,7 +109,7 @@ class Probabilidad():
                 if i==j:
                     ro = 1
                 else:
-                    ro = random.uniform(0.1,0.3)
+                    ro = random.uniform(0.8,1)
                 roFila.append(ro)
             roMatriz.append(roFila)
 
@@ -122,39 +130,44 @@ class Probabilidad():
     def obtenerColumna(self, matriz, column):
         return numpy.array(matriz)[:,column].tolist()
 
-    def graficar(self, xs, ys, zs, mu):
+    def graficar(self,DataSet, DataSetR, mu):
         from matplotlib.ticker import LinearLocator
+        xs = self.obtenerColumna(DataSet,0)
+        ys = self.obtenerColumna(DataSet,1)
+        zs = list(map(lambda a: 0, xs))
+
+        xr = self.obtenerColumna(DataSetR, 0)
+        yr = self.obtenerColumna(DataSetR, 1)
+        zr = list(map(lambda a: 0, xr))
+
         fig = plt.figure()
         ax = fig.gca(projection='3d')
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(xs, ys, zs)
-        #ax.set_zlim([0,1])
+        ax.scatter(xs, ys, zs, color='green')
 
-        puntosFuncion = self.__generarMatrizFuncion()
+        ax.scatter(xr, yr, zr, color='red')
+        #ax.set_zlim([0,1.1])
 
         z = []
 
         x = numpy.arange(0, 1, 0.01)
         y = numpy.arange(0, 1, 0.01)
 
+        puntosFuncion = self.__generarMatrizFuncion()
+
         x,y = numpy.meshgrid(x,y)
-        print(x)
-        
-        for i in range(0,len(puntosFuncion)):
-            res = self.pmf(puntosFuncion[i],mu,self.matrizCov)
-            z.append(min(1,res))
 
         for i in range(0,len(puntosFuncion)):
-            if z[i]>1:
-                print(x[i],y[i],z[i])
+            res = self.pdf(puntosFuncion[i],mu,self.matrizCov)
+            z.append(res)
 
         z = numpy.array(z)
         z = z.reshape((len(x), len(y)))
-        ax.contour3D(x, y, z, 50, cmap='binary')
 
+        ax.plot_surface(y, x, z, rstride=8, cstride=8, alpha=0.8)
 
         plt.show()
-    
+
     def __generarMatrizFuncion(self):
         datos = []
         x = numpy.arange(0, 1, 0.01).tolist()
@@ -164,47 +177,37 @@ class Probabilidad():
                 datos.append([i,j])
         return datos
 
-    def graficarEnsayo(self):
-        from matplotlib.ticker import LinearLocator
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
+    def decodificar(self, x):
+        if x == 'A':
+            return random.uniform(0.8, 1)
+        elif x == 'B':
+            return random.uniform(-0.1, 0.1)
+        elif x == 'I':
+            return -random.uniform(0.8, 1)
 
-        # Make data.
-        X = numpy.arange(-5, 5, 0.25)
-        xlen = len(X)
-        Y = numpy.arange(-5, 5, 0.25)
-        ylen = len(Y)
-        X, Y = numpy.meshgrid(X, Y)
-        R = numpy.sqrt(X**2 + Y**2)
-        Z = numpy.sin(R)
+    def archivo(self, DataSet:list):
+        import csv
 
-        # Create an empty array of strings with the same shape as the meshgrid, and
-        # populate it with two colors in a checkerboard pattern.
-        colortuple = ('y', 'b')
-        colors = numpy.empty(X.shape, dtype=str)
-        for y in range(ylen):
-            for x in range(xlen):
-                colors[x, y] = colortuple[(x + y) % len(colortuple)]
-
-        # Plot the surface with face colors taken from the array we made.
-        surf = ax.plot_surface(X, Y, Z, facecolors=colors, linewidth=0)
-
-        # Customize the z axis.
-        ax.set_zlim(-1, 1)
-        ax.w_zaxis.set_major_locator(LinearLocator(6))
-
-        plt.show()
+        with open('tabla.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(DataSet)
 
 
 p = Probabilidad()
+
 mu = [0,0]
-DataSet = p.generarDataSet(mu,[0.1,0.1],1000,2)
-#print(DataSet)
-#print(p.calcularPromedio(DataSet))
+sigma = [0.1,0.1]
+#combinatoria(n,2) = longitud
+ro = ["B","B","B"]
+##############################
+matrizCov = p.generarMatriz(sigma,ro)
 
-xs = p.obtenerColumna(DataSet,0)
-ys = p.obtenerColumna(DataSet,1)
-zs = list(map(lambda a: 0, xs))
+# Semilla = 2. Funciona
 
-p.graficar(xs,ys,zs,mu)
-#p.graficarEnsayo()
+DataSet, DataSetR = p.generarDataSet(mu,matrizCov,1000,2)
+
+p.archivo(DataSet)
+
+print(p.calcularPromedio(DataSet))
+if len(mu)==2:
+    p.graficar(DataSet, DataSetR,mu)
